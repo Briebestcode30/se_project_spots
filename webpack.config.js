c4842ccbfab6d2a707d2d2905dfff3d531a3d667
Briebestcode30 <brieanaharris033@penguin.com>
2025-12-02 14:37:30 -0500

    project changes

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,11 @@ module.exports = {
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
     ],
