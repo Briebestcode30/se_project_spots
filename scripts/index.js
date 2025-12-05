ec094f6d1ba2889911ad63e33631d4cb8b4c3ded
Briebestcode30 <brieanaharris033@penguin.com>
2025-12-04 19:43:50 -0500

    project changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,151 +1,154 @@
+import "./styles.css";
 import { settings, enableValidation, resetValidation } from "./validation.js";
 import Api from "./Api.js";
 
-const api = new Api({
-  baseUrl: "https://around-api.en.tripleten-services.com/v1",
-  headers: {
-    authorization: "f9c36c82-4848-4bdf-9403-6c4b1fd1762a",
-    "Content-Type": "application/json",
-  },
-});
-
-const editProfileBtn = document.querySelector(".profile__edit-btn");
-const editProfileModal = document.querySelector("#edit-profile-modal");
-const profileForm = document.querySelector("#profile-form");
-const profileNameInput = document.querySelector("#profile-name-input");
-const profileDescInput = document.querySelector("#profile-description-input");
-const profileNameEl = document.querySelector(".profile__name");
-const profileDescEl = document.querySelector(".profile__description");
-const profileAvatarEl = document.querySelector(".profile__avatar");
-
-const newPostBtn = document.querySelector(".profile__add-btn");
-const newPostModal = document.querySelector("#new-post-modal");
-const newPostForm = document.querySelector("#new-post-form");
-const newPostImageInput = document.querySelector("#card-image-input");
-const newPostCaptionInput = document.querySelector("#card-caption-input");
-
-const previewModal = document.querySelector("#preview-image-modal");
-const previewImage = previewModal.querySelector(".modal__image");
-const previewCaption = previewModal.querySelector(".modal__caption");
-
-const cardsContainer = document.querySelector(".cards__list");
-const cardTemplate = document.querySelector("#card-template");
-
-function openModal(modal) {
-  modal.classList.add("modal_opened");
-  document.addEventListener("keydown", handleEscClose);
-}
-
-function closeModal(modal) {
-  modal.classList.remove("modal_opened");
-  document.removeEventListener("keydown", handleEscClose);
-}
-
-function handleEscClose(evt) {
-  if (evt.key === "Escape") {
-    const openModalEl = document.querySelector(".modal_opened");
-    if (openModalEl) closeModal(openModalEl);
-  }
-}
-
-document.querySelectorAll(".modal").forEach((modal) => {
-  modal.addEventListener("mousedown", (evt) => {
-    if (evt.target === modal) closeModal(modal);
+document.addEventListener("DOMContentLoaded", () => {
+  const api = new Api({
+    baseUrl: "https://around-api.en.tripleten-services.com/v1",
+    headers: {
+      authorization: "f9c36c82-4848-4bdf-9403-6c4b1fd1762a",
+      "Content-Type": "application/json",
+    },
   });
-});
 
-document.querySelectorAll(".modal__close-btn").forEach((btn) => {
-  btn.addEventListener("click", () => closeModal(btn.closest(".modal")));
-});
+  const editProfileBtn = document.querySelector(".profile__edit-btn");
+  const editProfileModal = document.querySelector("#edit-profile-modal");
+  const profileForm = document.querySelector("#profile-form");
+  const profileNameInput = document.querySelector("#profile-name-input");
+  const profileDescInput = document.querySelector("#profile-description-input");
+  const profileNameEl = document.querySelector(".profile__name");
+  const profileDescEl = document.querySelector(".profile__description");
+  const profileAvatarEl = document.querySelector(".profile__avatar");
 
-function renderLoading(button, isLoading, defaultText) {
-  button.textContent = isLoading ? "Saving..." : defaultText;
-}
+  const newPostBtn = document.querySelector(".profile__add-btn");
+  const newPostModal = document.querySelector("#new-post-modal");
+  const newPostForm = document.querySelector("#new-post-form");
+  const newPostImageInput = document.querySelector("#card-image-input");
+  const newPostCaptionInput = document.querySelector("#card-caption-input");
 
-function getCardElement(data) {
-  const cardElement = cardTemplate.content
-    .querySelector(".card")
-    .cloneNode(true);
-  const cardTitle = cardElement.querySelector(".card__title");
-  const cardImage = cardElement.querySelector(".card__image");
+  const previewModal = document.querySelector("#preview-image-modal");
+  const previewImage = previewModal.querySelector(".modal__image");
+  const previewCaption = previewModal.querySelector(".modal__caption");
 
-  cardTitle.textContent = data.name;
-  cardImage.src = data.link;
-  cardImage.alt = data.name;
+  const cardsContainer = document.querySelector(".cards__list");
+  const cardTemplate = document.querySelector("#card-template");
 
-  cardImage.addEventListener("click", () => openImagePreview(data));
-  return cardElement;
-}
+  function openModal(modal) {
+    modal.classList.add("modal_opened");
+    document.addEventListener("keydown", handleEscClose);
+  }
 
-function openImagePreview(data) {
-  previewImage.src = data.link;
-  previewImage.alt = data.name;
-  previewCaption.textContent = data.name;
-  openModal(previewModal);
-}
+  function closeModal(modal) {
+    modal.classList.remove("modal_opened");
+    document.removeEventListener("keydown", handleEscClose);
+  }
 
-editProfileBtn.addEventListener("click", () => {
-  profileNameInput.value = profileNameEl.textContent;
-  profileDescInput.value = profileDescEl.textContent;
-  resetValidation(profileForm, settings);
-  openModal(editProfileModal);
-});
+  function handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      const openModalEl = document.querySelector(".modal_opened");
+      if (openModalEl) closeModal(openModalEl);
+    }
+  }
 
-profileForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const submitBtn = profileForm.querySelector(".modal__submit-btn");
-  renderLoading(submitBtn, true, "Save");
-
-  api
-    .updateProfile({
-      name: profileNameInput.value,
-      about: profileDescInput.value,
-    })
-    .then((res) => {
-      profileNameEl.textContent = res.name;
-      profileDescEl.textContent = res.about;
-      closeModal(editProfileModal);
-    })
-    .catch(console.error)
-    .finally(() => renderLoading(submitBtn, false, "Save"));
-});
-
-newPostBtn.addEventListener("click", () => {
-  resetValidation(newPostForm, settings);
-  openModal(newPostModal);
-});
-
-newPostForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const submitBtn = newPostForm.querySelector(".modal__submit-btn");
-  renderLoading(submitBtn, true, "Create");
-
-  api
-    .addCard({
-      name: newPostCaptionInput.value,
-      link: newPostImageInput.value,
-    })
-    .then((newCard) => {
-      const cardEl = getCardElement(newCard);
-      cardsContainer.prepend(cardEl);
-      newPostForm.reset();
-      closeModal(newPostModal);
-    })
-    .catch(console.error)
-    .finally(() => renderLoading(submitBtn, false, "Create"));
-});
-
-Promise.all([api.getUserInfo(), api.getInitialCards()])
-  .then(([userData, cards]) => {
-    profileNameEl.textContent = userData.name;
-    profileDescEl.textContent = userData.about;
-    profileAvatarEl.src = userData.avatar;
-
-    cards.reverse().forEach((card) => {
-      const cardEl = getCardElement(card);
-      cardsContainer.prepend(cardEl);
+  document.querySelectorAll(".modal").forEach((modal) => {
+    modal.addEventListener("mousedown", (evt) => {
+      if (evt.target === modal) closeModal(modal);
     });
-  })
-  .catch(console.error);
+  });
 
-enableValidation(settings);
+  document.querySelectorAll(".modal__close-btn").forEach((btn) => {
+    btn.addEventListener("click", () => closeModal(btn.closest(".modal")));
+  });
+
+  function renderLoading(button, isLoading, defaultText) {
+    button.textContent = isLoading ? "Saving..." : defaultText;
+  }
+
+  function getCardElement(data) {
+    const cardElement = cardTemplate.content
+      .querySelector(".card")
+      .cloneNode(true);
+    const cardTitle = cardElement.querySelector(".card__title");
+    const cardImage = cardElement.querySelector(".card__image");
+
+    cardTitle.textContent = data.name;
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+
+    cardImage.addEventListener("click", () => openImagePreview(data));
+    return cardElement;
+  }
+
+  function openImagePreview(data) {
+    previewImage.src = data.link;
+    previewImage.alt = data.name;
+    previewCaption.textContent = data.name;
+    openModal(previewModal);
+  }
+
+  editProfileBtn.addEventListener("click", () => {
+    profileNameInput.value = profileNameEl.textContent;
+    profileDescInput.value = profileDescEl.textContent;
+    resetValidation(profileForm, settings);
+    openModal(editProfileModal);
+  });
+
+  profileForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const submitBtn = profileForm.querySelector(".modal__submit-btn");
+    renderLoading(submitBtn, true, "Save");
+
+    api
+      .updateProfile({
+        name: profileNameInput.value,
+        about: profileDescInput.value,
+      })
+      .then((res) => {
+        profileNameEl.textContent = res.name;
+        profileDescEl.textContent = res.about;
+        closeModal(editProfileModal);
+      })
+      .catch(console.error)
+      .finally(() => renderLoading(submitBtn, false, "Save"));
+  });
+
+  newPostBtn.addEventListener("click", () => {
+    resetValidation(newPostForm, settings);
+    openModal(newPostModal);
+  });
+
+  newPostForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const submitBtn = newPostForm.querySelector(".modal__submit-btn");
+    renderLoading(submitBtn, true, "Create");
+
+    api
+      .addCard({
+        name: newPostCaptionInput.value,
+        link: newPostImageInput.value,
+      })
+      .then((newCard) => {
+        const cardEl = getCardElement(newCard);
+        cardsContainer.prepend(cardEl);
+        newPostForm.reset();
+        closeModal(newPostModal);
+      })
+      .catch(console.error)
+      .finally(() => renderLoading(submitBtn, false, "Create"));
+  });
+
+  Promise.all([api.getUserInfo(), api.getInitialCards()])
+    .then(([userData, cards]) => {
+      profileNameEl.textContent = userData.name;
+      profileDescEl.textContent = userData.about;
+      profileAvatarEl.src = userData.avatar;
+
+      cards.reverse().forEach((card) => {
+        const cardEl = getCardElement(card);
+        cardsContainer.prepend(cardEl);
+      });
+    })
+    .catch(console.error);
+
+  enableValidation(settings);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,46 +4,50 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./scripts/index.js",
+  entry: {
+    main: "./scripts/index.js",
+  },
 
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "se_spots_sprint4"),
     filename: "main.[contenthash].js",
     assetModuleFilename: "assets/[hash][ext][query]",
+    publicPath: "",
     clean: true,
   },
 
   mode: "development",
+  devtool: "inline-source-map",
+  stats: "errors-only",
 
   devServer: {
-    static: path.join(__dirname, "dist"),
-    port: 3000,
+    static: path.resolve(__dirname, "se_spots_sprint4"),
+    compress: true,
+    port: 8080,
     open: true,
     hot: true,
+    liveReload: true,
   },
+
+  target: ["web", "es5"],
 
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        loader: "babel-loader",
       },
-
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
-
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
         type: "asset/resource",
       },
-
       {
-        test: /\.(woff|woff2|ttf|eot)$/i,
+        test: /\.(woff|woff2|ttf|eot|otf)$/i,
         type: "asset/resource",
       },
     ],
@@ -51,11 +55,9 @@ module.exports = {
 
   plugins: [
     new CleanWebpackPlugin(),
-
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: "./src/index.html",
     }),
-
     new MiniCssExtractPlugin({
       filename: "styles.[contenthash].css",
     }),
