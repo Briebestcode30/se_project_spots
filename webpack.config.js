1ffcca9a7d081a4cec09d4aed17d221d4261a404
Briebestcode30 <brieanaharris033@penguin.com>
2025-12-05 16:59:20 -0500

    project changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,57 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./scripts/index.js", // your main JS file
+  entry: {
+    main: "./scripts/index.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.[contenthash].js",
-    assetModuleFilename: "images/[name].[hash][ext][query]", // images go to dist/images
+    filename: "main.js",
     publicPath: "",
-    clean: true, // clean dist before each build
+    assetModuleFilename: "images/[name][ext][query]",
   },
-
   mode: "development",
   devtool: "inline-source-map",
   stats: "errors-only",
-
   devServer: {
-    static: path.resolve(__dirname, "dist"),
+    static: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
     open: true,
-    hot: true,
     liveReload: true,
+    hot: false,
   },
-
+  target: ["web", "es5"],
+  resolve: {
+    alias: {
+      images: path.resolve(__dirname, "src/images"),
+    },
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
+        loader: "babel-loader",
         exclude: /node_modules/,
-        use: "babel-loader",
       },
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource", // handle images correctly
-        generator: {
-          filename: "images/[name].[hash][ext]", // output folder for images
-        },
-      },
-      {
-        test: /\.(woff|woff2|ttf|eot|otf)$/i,
-        type: "asset/resource", // fonts
-        generator: {
-          filename: "fonts/[name].[hash][ext]",
-        },
+        test: /\.(png|svg|jpg|jpeg|webp|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
       },
     ],
   },
-
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: "./src/index.html",
     }),
-    new MiniCssExtractPlugin({
-      filename: "styles.[contenthash].css",
-    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
   ],
-
-  resolve: {
-    extensions: [".js"], // resolve JS files automatically
-  },
 };
